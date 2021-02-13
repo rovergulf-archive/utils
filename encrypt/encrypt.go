aceea70320b1d3c4822afb815fb367d609986bff
Dmitrii Limonov <disordervlz@gmail.com>
2021-02-14 01:55:08 +0300

    go mod tidy remove exit package

--- encrypt/encrypt.go
+++ encrypt/encrypt.go
@@ -6,13 +6,9 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"github.com/rovergulf/utils/clog"
+	"go.uber.org/zap"
 	"golang.org/x/crypto/sha3"
 	"io"
-)
-
-const (
-	key = "qhA2KF6q8twuPyXj"
 )
 
 func EncodeSHA3String(v string) string {
@@ -21,38 +17,38 @@ func EncodeSHA3String(v string) string {
 	return hex.EncodeToString(hash.Sum(nil))
 }
 
-func Encrypt(plaintext []byte, key []byte) ([]byte, error) {
+func Encrypt(lg *zap.SugaredLogger, plaintext []byte, key []byte) ([]byte, error) {
 	c, err := aes.NewCipher(key)
 	if err != nil {
-		clog.Error(err)
+		lg.Error(err)
 		return nil, err
 	}
 
 	gcm, err := cipher.NewGCM(c)
 	if err != nil {
-		clog.Error(err)
+		lg.Error(err)
 		return nil, err
 	}
 
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		clog.Error(err)
+		lg.Error(err)
 		return nil, err
 	}
 
 	return gcm.Seal(nonce, nonce, plaintext, nil), nil
 }
 
-func Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
+func Decrypt(lg *zap.SugaredLogger, ciphertext []byte, key []byte) ([]byte, error) {
 	c, err := aes.NewCipher(key)
 	if err != nil {
-		clog.Error(err)
+		lg.Error(err)
 		return nil, err
 	}
 
 	gcm, err := cipher.NewGCM(c)
 	if err != nil {
-		clog.Error(err)
+		lg.Error(err)
 		return nil, err
 	}
 
--- go.mod
+++ go.mod
@@ -7,10 +7,6 @@ replace github.com/rovergulf/utils => github.com/rovergulf/utils v0.2.1
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.0.1 // indirect
 	github.com/Shopify/sarama v1.27.2
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/websocket v1.4.2
-	github.com/grpc-ecosystem/grpc-gateway v1.14.6
-	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645
 	github.com/jackc/pgconn v1.8.0
 	github.com/jackc/pgx/v4 v4.10.1
 	github.com/nats-io/nats-streaming-server v0.19.0 // indirect
@@ -18,12 +14,10 @@ require (
 	github.com/nats-io/stan.go v0.7.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/prometheus/client_golang v1.9.0
-	github.com/sirupsen/logrus v1.7.0
 	github.com/uber/jaeger-client-go v2.25.0+incompatible
 	github.com/uber/jaeger-lib v2.4.0+incompatible
 	go.uber.org/zap v1.16.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
-	golang.org/x/net v0.0.0-20201224014010-6772e930b67b
-	google.golang.org/grpc v1.29.1
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
